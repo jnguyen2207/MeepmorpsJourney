6fd89301cf1675218b069a2402ffa52f1595af0b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-23 10:49:20 +1200

    a

--- scenes/Panel.gd
+++ scenes/Panel.gd
@@ -2,6 +2,12 @@ extends CanvasLayer
 
 var activation_steps = 0
 
+func check_activation_complete():
+	if activation_steps >= 5:
+		# Keycard is now activated
+		$keycardactivationmachine._exit_activation_menu()  # Close the menu
+		Global.keycard_activated = true  # Mark the keycard as activated in your global state
+
 func _on_Button1_pressed():
 	activation_steps += 1
 	check_activation_complete()
@@ -21,9 +27,3 @@ func _on_Button4_pressed():
 func _on_Button5_pressed():
 	activation_steps += 1
 	check_activation_complete()
-
-func check_activation_complete():
-	if activation_steps >= 5:
-		# Keycard is now activated
-		$keycardactivationmachine._exit_activation_menu()  # Close the menu
-		Global.keycard_activated = true  # Mark the keycard as activated in your global state
--- scenes/activationmachine.gd
+++ scenes/activationmachine.gd
@@ -4,12 +4,9 @@ extends Area3D
 @export var player_path: NodePath
 var player: Node3D
 
-func check_activation_complete() -> bool:
-	# Example logic to check if all buttons have been pressed
-	for button in activation_menu.get_node("Buttons").get_children():
-		if button is Button and not button.pressed:
-			return false
-	return true
+var other_node = get_node("keycardactivationmachine/activation_menu")
+if other_node.check_activation_complete():
+	# do something
 
 func _ready():
 	player = get_node(player_path)
@@ -18,11 +15,8 @@ func _ready():
 			if button is Button:
 				button.connect("pressed", Callable(self, "_on_button_pressed"))
 	# Assume keycard script is at a known path or instance
-	var keycard = get_node("key/key2")
-	if keycard != null:
-		keycard.connect("keycard_activated", Callable(self, "_exit_activation_menu"))
-	else:
-		print("Keycard node not found!")
+	var keycard = get_node("key/key2")  # Adjust the path as necessary
+	keycard.connect("keycard_activated", Callable(self, "_exit_activation_menu"))
 
 func _on_button_pressed():
 	# Check if all buttons have been pressed or perform any action
