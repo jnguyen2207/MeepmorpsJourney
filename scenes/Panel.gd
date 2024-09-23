6ae95fd4068cd1f9995feecdb4a2794c5f13d544
jnguyen2207 <nguyenjack2207@gmail.com>
2024-09-23 22:32:42 +1200

    a

--- scenes/Panel.gd
+++ scenes/Panel.gd
@@ -1,12 +1,12 @@
 extends CanvasLayer
 
-var activation_steps = 0
-
 func check_activation_complete():
 	if activation_steps >= 5:
 		# Keycard is now activated
 		$keycardactivationmachine._exit_activation_menu()  # Close the menu
 		Global.keycard_activated = true  # Mark the keycard as activated in your global state
+
+var activation_steps = 0
 
 func _on_Button1_pressed():
 	activation_steps += 1
--- scenes/activationmachine.gd
+++ scenes/activationmachine.gd
@@ -4,25 +4,32 @@ extends Area3D
 @export var player_path: NodePath
 var player: Node3D
 
-var other_node = get_node("keycardactivationmachine/activation_menu")
-if other_node.check_activation_complete():
-	# do something
+# This function is called when a button is pressed
+func _on_button_pressed():
+	# Check if activation_menu has the method "check_activation_complete"
+	if activation_menu.has_method("check_activation_complete"):
+		if activation_menu.check_activation_complete():
+			_exit_activation_menu()
+	else:
+		print("The method 'check_activation_complete' is not found in activation_menu.")
 
+# Ready function to set up initial settings
 func _ready():
 	player = get_node(player_path)
 	if activation_menu != null:
+		# Connect all buttons in the "Buttons" node to the "_on_button_pressed" function
 		for button in activation_menu.get_node("Buttons").get_children():
 			if button is Button:
 				button.connect("pressed", Callable(self, "_on_button_pressed"))
+
 	# Assume keycard script is at a known path or instance
 	var keycard = get_node("key/key2")  # Adjust the path as necessary
-	keycard.connect("keycard_activated", Callable(self, "_exit_activation_menu"))
+	if keycard and keycard.has_method("keycard_activated"):
+		keycard.connect("keycard_activated", Callable(self, "_exit_activation_menu"))
+	else:
+		print("Keycard node or method 'keycard_activated' not found.")
 
-func _on_button_pressed():
-	# Check if all buttons have been pressed or perform any action
-	if check_activation_complete():
-		_exit_activation_menu()
-
+# Exit the activation menu when all buttons are pressed
 func _exit_activation_menu():
 	if activation_menu:
 		activation_menu.visible = false
