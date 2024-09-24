014e2d43a44abd5f9cece29d8d6293e8ae8323f5
jnguyen2207 <nguyenjack2207@gmail.com>
2024-09-24 23:01:19 +1200

    a

--- scripts/activationmachine.gd
+++ scripts/activationmachine.gd
@@ -3,6 +3,7 @@ extends Area3D
 @export var activation_menu: CanvasLayer
 @export var player_path: NodePath
 var player: Node3D
+var keycard_collected = false  # New variable to track keycard collection
 
 # This function is called when a button is pressed
 func _on_button_pressed():
@@ -13,13 +14,6 @@ func _on_button_pressed():
 	else:
 		print("The method 'check_activation_complete' is not found in activation_menu.")
 
-# Exit the activation menu when all buttons are pressed
-func _exit_activation_menu():
-	if activation_menu:
-		activation_menu.visible = false
-		get_tree().paused = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 # Ready function to set up initial settings
 func _ready():
 	player = get_node(player_path)
@@ -28,10 +22,31 @@ func _ready():
 		for button in activation_menu.get_node("Buttons").get_children():
 			if button is Button:
 				button.connect("pressed", Callable(self, "_on_button_pressed"))
-
+	
 	# Assume keycard script is at a known path or instance
-	var keycard = get_node("key/key2")  # Adjust the path as necessary
+	var keycard = get_node("activation_menu/key/key2")  # Adjust the path as necessary
 	if keycard and keycard.has_method("keycard_activated"):
-		keycard.connect("keycard_activated", Callable(self, "_exit_activation_menu"))
+		keycard.connect("keycard_activated", Callable(self, "_on_keycard_collected"))
 	else:
 		print("Keycard node or method 'keycard_activated' not found.")
+
+# New function to handle keycard collection
+func _on_keycard_collected():
+	keycard_collected = true  # Set to true when the keycard is collected
+	print("Keycard collected!")
+
+# Function to handle collision or interaction with the activation machine
+func _on_collision_with_activation_machine():
+	if keycard_collected:  # Only open the activation menu if keycard is collected
+		activation_menu.visible = true
+		get_tree().paused = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		print("Keycard not collected yet.")
+
+# Exit the activation menu when all buttons are pressed
+func _exit_activation_menu():
+	if activation_menu:
+		activation_menu.visible = false
+		get_tree().paused = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- scripts/key.gd
+++ scripts/key.gd
@@ -2,8 +2,7 @@ extends Area3D
 
 # Signal to notify when the keycard is picked up
 signal picked_up
-
-signal keycard_activated
+signal keycard_activated  # Signal emitted when keycard is collected
 
 # Variables to control the bobbing motion
 var bobbing_speed = 3.5
@@ -13,34 +12,39 @@ var initial_position
 # Rotation speed
 var rotation_speed = Vector3(0, 1, 0)  # Rotating around the Y-axis
 
+# Called when the node is added to the scene
 func _ready():
-	# Emit the signal when the keycard is activated
-	emit_signal("keycard_activated")
 	initial_position = global_transform.origin
+
+	# If the keycard was already picked up, remove it
 	if Global.keycard_picked_up:
 		queue_free()  # The keycard was already picked up, so remove it
 
+# Called every frame, used for animation
 func _process(delta):
 	# Rotate and bob the keycard
 	rotate_object_local(Vector3(0, 1, 0), rotation_speed.y * delta)
 	var new_y = initial_position.y + sin(Time.get_ticks_msec() / 1000.0 * bobbing_speed) * bobbing_height
 	global_transform.origin.y = new_y
 
+# When the player interacts with the keycard
 func _on_body_entered(body):
 	if body.name == "player":
-		#Plays sound fx
+		# Play sound fx
 		AudioManager.play("res://soundfx/click.mp3")
 		
-		#Tells the game that the kaycard has been picked up
+		# Notify that the keycard has been picked up
 		Global.keycard_picked_up = true
 		
-		#emit signal and removes the original keycard
+		# Emit signal for keycard being picked up
 		emit_signal("picked_up")
-		get_parent().queue_free()
 		
+		# Activate the keycard and notify the activation machine
+		activate_keycard()
 
-func _on_picked_up():
-	activate_keycard()
+		# Remove the keycard from the scene
+		get_parent().queue_free()
 
+# Activate the keycard, this will emit the 'keycard_activated' signal
 func activate_keycard():
 	emit_signal("keycard_activated")
